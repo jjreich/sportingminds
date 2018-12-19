a7199993dc60503e09f56fb41175ef56608fdf42
jjreich <jj.reich@nalusolutions.com>
2018-12-19 16:26:21 -0600

    Added rolify gem to the application

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  rolify :before_add => :before_add_method
+
+  after_create :assign_default_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def before_add_method(role)
+
+  end
+
+  def assigne_default_role
+  	self.add_role(:athlete) if self.roles.blank?
+  end
 end
