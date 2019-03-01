4b0ca36e942daf074996a21d5bfe6790a1dff1b7
jjreich <jj.reich@nalusolutions.com>
2019-03-01 16:26:48 -0600

    trying to fix the problem with accessing redis on heroku.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 # Establish a connection between Resque and Redis
 if Rails.env == "production"
-  uri = URI.parse ENV["REDISCLOUD_URL"]
+  uri = URI.parse ENV["REDIS_URL"]
   Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
 else
   # conf for your localhost
