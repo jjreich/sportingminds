02fa4ebbf59352de449d71fd82b28009a70ad667
jjreich <jj.reich@nalusolutions.com>
2019-03-02 17:25:56 -0600

    cleaning up the sidekiq switch

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,7 +1,7 @@
 # Establish a connection between Resque and Redis
-if Rails.env == "production"
-  uri = URI.parse ENV["REDIS_URL"]
-  Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
-else
+# if Rails.env == "production"
+#  uri = URI.parse ENV["REDIS_URL"]
+#  Resque.redis = Redis.new host:uri.host, port:uri.port, password:uri.password
+# else
   # conf for your localhost
-end
+# end
